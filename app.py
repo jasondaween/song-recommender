748a227b952da85b83aeb407218b161769f03624
Jason Yang <jasony0613@gmail.com>
2026-04-09 19:59:57 -0400

    Update styles.css to display the song aesthetically

--- app.py
+++ app.py
@@ -5,27 +5,27 @@ songs = {
     "Pop": [
         {
             "name": "Die With A Smile", "artist": "Lady Gaga & Bruno Mars", 
-            "img": "https://i.scdn.co/image/ab67616d00001e0282ea2e9e1858aa012c57cd45", 
+            "img": "https://i.scdn.co/image/ab67616d0000b27382ea2e9e1858aa012c57cd45", 
             "link": "https://open.spotify.com/track/2plbrEY59IikOBgBGLjaoe?si=56374fbd314f4f9b"
         },
         {
             "name": "BIRDS OF A FEATHER", "artist": "Billie Eilish",
-            "img": "https://i.scdn.co/image/ab67616d0000485171d62ea7ea8a5be92d3c1f62",
+            "img": "https://i.scdn.co/image/ab67616d0000b27371d62ea7ea8a5be92d3c1f62",
             "link": "https://open.spotify.com/track/6dOtVTDdiauQNBQEDOtlAB?si=5608afebcf8e4c52"
         },
         {
             "name": "Espresso", "artist": "Sabrina Carpenter",
-            "img": "https://i.scdn.co/image/ab67616d00004851255ec9ddd8af81fd9aba2ced",
+            "img": "https://i.scdn.co/image/ab67616d0000b273659cd4673230913b3918e0d5",
             "link": "https://open.spotify.com/track/1vLqigPHwiFnXsfrLMehV1?si=14c317eac84c4c84"
         },
         {
             "name": "So Easy (To Fall In Love)", "artist": "Olivia Dean",
-            "img": "https://i.scdn.co/image/ab67616d00001e029a336bfb6d40bbd90a507417",
+            "img": "https://i.scdn.co/image/ab67616d0000b2739a336bfb6d40bbd90a507417",
             "link": "https://open.spotify.com/track/6sGIMrtIzQjdzNndVxe397?si=4185bcc356fc4e3b"
         },
         {
             "name": "thank u, next", "artist": "Ariana Grande",
-            "img": "https://i.scdn.co/image/ab67616d0000485156ac7b86e090f307e218e9c8",
+            "img": "https://i.scdn.co/image/ab67616d00001e0256ac7b86e090f307e218e9c8",
             "link": "https://open.spotify.com/track/3e9HZxeyfWwjeyPAMmWSSQ?si=e33bce6094f34ccb"
         }
     ]
